f54c516d21dbc520c39272e18282f228d2bac497
shawn <shawntan2@gmail.com>
2021-12-14 16:48:02 +0800

    final

--- assessment/src/main/java/tfipjava/HttpClientConnection.java
+++ assessment/src/main/java/tfipjava/HttpClientConnection.java
@@ -35,35 +35,32 @@ public class HttpClientConnection implements Runnable{
         }
 
 while (true){
-String inputFromClient=in.readLine();
-System.out.println(inputFromClient);
-Scanner scanForGet=new Scanner(inputFromClient);
-String s=scanForGet.next();
-String afterGet=scanForGet.next().trim();
-if (!s.equals("GET")) {
+        String inputFromClient=in.readLine();
+        System.out.println(inputFromClient);
+        Scanner scanForGet=new Scanner(inputFromClient);
+        String s=scanForGet.next();
+        String afterGet=scanForGet.next().trim();
+    if (!s.equals("GET")) {
     out.println("HTTP/1.1 405 Method Not Allowed\r\n\r\n <method name> not supported\r\n");
     out.flush();
-}
+    }
 else if (s.equals("GET")){
     File filecheck=new File(s+afterGet);
-if (!filecheck.exists())
-{
-    out.println("HTTP/1.1 404 Not Found\r\n\r\n <resource name> not found\r\n");
-    out.flush();
-    sock.close();
-}
-else{
-    if (afterGet.equals("/"))
-        {
-File fileChecker=new File(n);
-if (fileChecker.exists())
-            {   
-            String str=new String("HTTP/1.1 200 OK\r\n\r\n <resource contents as bytes>");
-            out.println(str.getBytes());
-           out.flush();
-            sock.close();       
-  }
-  }
+    if (!filecheck.exists()){
+        out.println("HTTP/1.1 404 Not Found\r\n\r\n <resource name> not found\r\n");
+        out.flush();
+        sock.close();
+    }
+    else{
+        if (afterGet.equals("/")) {
+            File fileChecker=new File(n);
+            if (fileChecker.exists()){   
+                String str=new String("HTTP/1.1 200 OK\r\n\r\n <resource contents as bytes>");
+                out.println(str.getBytes());
+                out.flush();
+                sock.close();       
+            }
+        }
         else
         {
             File filechecker =new File(afterGet);
@@ -73,17 +70,20 @@ if (fileChecker.exists())
                 sock.close();
             }
         }
-}
-}
 
+    }
+    }
+       
+        }
+    }
         catch(Exception e)
         {
-System.exit(1);
+        System.exit(1);
         }
     }
     }
-    }
-    }
+    
+ 
     
     
 
