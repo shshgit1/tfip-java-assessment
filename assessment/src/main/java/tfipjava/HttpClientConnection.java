5fe772f1455c7d3eff044ca697283181c647c295
shawn <shawntan2@gmail.com>
2021-12-14 16:40:17 +0800

    440

--- assessment/src/main/java/tfipjava/HttpClientConnection.java
+++ assessment/src/main/java/tfipjava/HttpClientConnection.java
@@ -1,18 +1,10 @@
 package tfipjava;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class HttpClientConnection implements Runnable{
@@ -27,40 +19,32 @@ public class HttpClientConnection implements Runnable{
         this.FDirectory=FileDirectory;
         this.moreFiles=moreFiles;
     }
-public static void main(String[] args) throws UnknownHostException, IOException {
-}
-@Override
-public void run()
+
+    @Override
+    public void run()
     {
         PrintWriter out = null;
         BufferedReader in = null;
-        try{            
-            InputStream inps=sock.getInputStream();
-            OutputStream osos=sock.getOutputStream();            
-            BufferedInputStream binps= new BufferedInputStream(inps);
-            DataInputStream dinps= new DataInputStream(binps);
-            BufferedOutputStream bos=new BufferedOutputStream(osos);
-            DataOutputStream dos=new DataOutputStream(bos);
+        try{       
+            
             out = new PrintWriter(sock.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             File f=new File(FDirectory);
-            if (!f.exists()){
+            if (!f.exists() || !f.isDirectory()){
             System.exit(1);
         }
 
-
+while (true){
 String inputFromClient=in.readLine();
 System.out.println(inputFromClient);
 Scanner scanForGet=new Scanner(inputFromClient);
 String s=scanForGet.next();
 String afterGet=scanForGet.next().trim();
-if (!s.equals("GET"))
-{
+if (!s.equals("GET")) {
     out.println("HTTP/1.1 405 Method Not Allowed\r\n\r\n <method name> not supported\r\n");
     out.flush();
 }
-else if (s.equals("GET"))
-{
+else if (s.equals("GET")){
     File filecheck=new File(s+afterGet);
 if (!filecheck.exists())
 {
@@ -83,20 +67,23 @@ if (fileChecker.exists())
         else
         {
             File filechecker =new File(afterGet);
-            if (filechecker.exists() && afterGet.toLowerCase().contains("png"))
-            {
+            if (filechecker.exists() && afterGet.toLowerCase().contains("png")){
                 out.println("HTTP/1.1 200 OK\r\n\r\n <resopurce contents as bytes>");
                 out.flush();
                 sock.close();
             }
-                    }
-}
-}
         }
+}
+}
+
         catch(Exception e)
         {
 System.exit(1);
         }
-    
     }
-}
+    }
+    }
+    }
+    
+    
+
